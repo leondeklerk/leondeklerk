b249998f268373b8572a7e11d6722e2a4301c4fc
Leon de Klerk <developer@leondeklerk.com>
2023-12-29 17:53:05 +0100

    Improve web performance
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
 					v-for="(link, index) in links"
 					:key="index"
 					:href="link.url"
+					:aria-label="link.text"
 					class="h-14 w-14 sm:h-16 sm:w-16 lg:h-20 lg:w-20 text-zinc-600 dark:text-zinc-300 bg-white dark:bg-zinc-900 shadow-sm hover:text-orange-400/80 dark:hover:text-orange-400 text-2xl sm:text-3xl rounded-xl flex items-center justify-center border border-slate-200 dark:border-zinc-600 hover:shadow-md duration-200"
 				>
 					<i :class="link.icon" />
@@ -25,16 +26,12 @@
 			<divider-component>Apps</divider-component>
 
 			<div class="flex flex-col md:flex-row justify-center gap-2">
-				<div v-for="(app, index) in apps" :key="index" class="m-auto grow w-2/3 md:w-full">
+				<div v-for="(app, index) in apps" :key="index" class="m-auto grow w-full md:w-full">
 					<a
 						:href="app.url"
 						class="text-zinc-600 dark:text-zinc-300 bg-white dark:bg-zinc-900 shadow-sm hover:text-orange-400/80 dark:hover:text-orange-400 rounded-xl flex items-center border border-slate-200 dark:border-zinc-600 hover:shadow-md duration-200 p-2 whitespace-nowrap"
 					>
-						<img
-							:alt="app.title"
-							:src="app.icon"
-							class="rounded-xl lg:w-14 lg:h-14 w-10 h-10 border shadow-sm mr-1 md:mr-3 dark:border-zinc-600"
-						/>
+						<img :src="app.icon" class="rounded-xl lg:w-14 lg:h-14 w-10 h-10 border shadow-sm mr-2 md:mr-3 dark:border-zinc-600" />
 						<p class="">{{ app.title }}</p>
 					</a>
 				</div>
@@ -44,40 +41,41 @@
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
 import DividerComponent from "./components/DividerComponent.vue";
 
-const links = reactive([
+const links = [
 	{
 		url: "https://github.com/leondeklerk",
 		icon: "fa-brands fa-github",
+		text: "GitHub",
 	},
 	{
 		url: "mailto:contact@leondeklerk.com",
 		icon: "fa-regular fa-envelope",
+		text: "E-mail",
 	},
-]);
+];
 
-const apps = reactive([
+const apps = [
 	{
 		url: "https://play.google.com/store/apps/details?id=com.leondeklerk.scored",
-		icon: "/scored.png",
+		icon: "/scored.webp",
 		title: "Scored",
 	},
 	{
 		url: "https://play.google.com/store/apps/details?id=com.leondeklerk.wheremybike",
-		icon: "/fietslocatie.png",
+		icon: "/fietslocatie.webp",
 		title: "Fiets locatie",
 	},
 	{
 		url: "https://play.google.com/store/apps/details?id=nl.leontheclerk.gyroblock",
-		icon: "/gyroblock.png",
+		icon: "/gyroblock.webp",
 		title: "Gyroblock",
 	},
 	{
 		url: "https://play.google.com/store/apps/details?id=nl.leontheclerk.when2leave",
-		icon: "/w2l.png",
+		icon: "/w2l.webp",
 		title: "When to Leave?",
 	},
-]);
+];
 </script>
