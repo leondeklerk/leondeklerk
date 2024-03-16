1e6350adb789d3a09bcb422b8f811c357f3aba38
Leon de Klerk <developer@leondeklerk.com>
2024-03-17 00:23:28 +0100

    Undo accidental change
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- src/components/DividerComponent.vue
+++ src/components/DividerComponent.vue
@@ -2,7 +2,7 @@
 	<div class="sm:w-1/2 w-3/4 m-auto flex py-5 items-center">
 		<div class="flex-grow border-t border-slate-400"></div>
 		<span class="flex-shrink mx-4 text-zinc-600 dark:text-zinc-300 uppercase">
-			<!-- <slot /> -->
+			<slot />
 		</span>
 		<div class="flex-grow border-t border-slate-400"></div>
 	</div>
